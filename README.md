a79859054b3dec43fdf47c2f8be7d8ed4d53c964
craig95 <u15029779@tuks.co.za>
2017-09-17 23:52:12 +0200

    Added parallax effects to backgrounds. Updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 2017_IMY320_MiniSonic
-IMY 320 Group Design and Development Assignment 
+IMY 320 Group Design and Development Assignment
+
+##The in progress website can be seen at:
+https://github.com/craig95/2017_IMY320_MiniSonic 
--- js/main.js
+++ js/main.js
@@ -1,36 +1,23 @@
-$(function(){
-    $("#page-wrap").wrapInner("<table cellspacing='30'><tr>");
-    $(".post").wrap("<td></td>");
-    $(".post").width(Math.max(document.documentElement.clientWidth, window.innerWidth || 0)-100);
-    $("body").mousewheel(function(event, delta) {
-        this.scrollLeft -= (delta * 30);
-        event.preventDefault();
-    });
-});
-
 var lastScrollLeft = 0;
-$(function(){
-    $(window).scroll(function(event){
-        var st = $(this).scrollLeft();
-        console.log(st);
-        console.log(lastScrollLeft);
-        if (st > lastScrollLeft) {
-            if ($('#character').attr("src") != "img/walking.gif") {
-                $('#character').attr("src", "img/walking.gif");
-            }
-            clearTimeout( $.data( this, "scrollCheck" ) );
-            $.data( this, "scrollCheck", setTimeout(function() {
-                $('#character').attr("src", "img/standing.gif");
-            }, 200) );
-        } else {
-            if ($('#character').attr("src") != "img/walking_back.gif") {
-                $('#character').attr("src", "img/walking_back.gif");
-            }
-            clearTimeout( $.data( this, "scrollCheck" ) );
-            $.data( this, "scrollCheck", setTimeout(function() {
-                $('#character').attr("src", "img/standing_back.gif");
-            }, 200) );
+$(window).scroll(function(event){
+    var st = $(this).scrollLeft();
+    if (st > lastScrollLeft) {
+        console.log($('#character').css("background-image"));
+        if ($('#character').css("background-image") != 'url("img/walking.gif")') {
+            $('#character').css("background-image", 'url("img/walking.gif")');
         }
-        lastScrollLeft = st;
-    });
+        clearTimeout($.data(this, "scrollCheck"));
+        $.data(this, "scrollCheck", setTimeout(function () {
+            $('#character').css("background-image", 'url("img/standing.png")');
+        }, 200));
+    } else {
+        if ($('#character').css("background-image") != 'url("../img/walking_back.gif")') {
+            $('#character').css("background-image", 'url("img/walking_back.gif")');
+        }
+        clearTimeout($.data(this, "scrollCheck"));
+        $.data(this, "scrollCheck", setTimeout(function () {
+            $('#character').css("background-image", 'url("img/standing_back.png")');
+        }, 100));
+    }
+    lastScrollLeft = st;
 });
