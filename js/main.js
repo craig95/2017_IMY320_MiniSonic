0358b01c454cc8e2f195c4f5c7f3a978f9eb9452
craig95 <u15029779@tuks.co.za>
2017-09-18 10:46:40 +0200

    Added floating Islands

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var lastScrollLeft = 0;
 $(window).scroll(function(event){
     var st = $(this).scrollLeft();
     if (st > lastScrollLeft) {
-        console.log($('#character').css("background-image"));
         if ($('#character').css("background-image") != 'url("img/walking.gif")') {
             $('#character').css("background-image", 'url("img/walking.gif")');
         }
@@ -21,3 +20,33 @@ $(window).scroll(function(event){
     }
     lastScrollLeft = st;
 });
+
+setTimeout(function island1() {
+    $('#island-1').animate({
+        'background-position-y': '42%'
+    }, 2000, 'linear');
+    $('#island-1').animate({
+        'background-position-y': '40%'
+    }, 2000, 'linear');
+    setTimeout(island1, Math.floor((Math.random() * 1000) + 1));
+}, Math.floor((Math.random() * 1000) + 1));
+
+setTimeout(function island2() {
+    $('#island-2').animate({
+        'background-position-y': '48%'
+    }, 2000, 'linear');
+    $('#island-2').animate({
+        'background-position-y': '50%'
+    }, 2000, 'linear');
+    setTimeout(island2, Math.floor((Math.random() * 1000) + 1));
+}, Math.floor((Math.random() * 1000) + 1));
+
+setTimeout(function island3() {
+    $('#island-3').animate({
+        'background-position-y': '22%'
+    }, 2000, 'linear');
+    $('#island-3').animate({
+        'background-position-y': '20%'
+    }, 2000, 'linear');
+    setTimeout(island3, Math.floor((Math.random() * 1000) + 1));
+}, Math.floor((Math.random() * 1000) + 1));
